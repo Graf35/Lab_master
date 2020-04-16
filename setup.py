b6193d7db61ceb60e5a5ca6959a713b00549f584
Graf_35 <Graf3456718@yandex.ru>
2020-04-16 18:39:06 +0300

    Lab_2 debug

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ build_exe_options = {"includes":[ 'lab\Lab_1\Calculetion', "lab\Lab_1\File",  "l
                                   'lab\Lab_2\Calculetion', "lab\Lab_2\File",  "lab\Lab_2\Log", "lab\Lab_2\office",  "lab\Lab_2\Window2",
                                   'lab\Lab_3\Calculetion', "lab\Lab_3\File",  "lab\Lab_3\Log", "lab\Lab_3\office",  "lab\Lab_3\Window3",
                                   'lab\Lab_4\Calculetion', "lab\Lab_4\File",  "lab\Lab_4\Log", "lab\Lab_4\office",  "lab\Lab_4\Window4",
-                                  'lab\Lab_5\Calculetion', "lab\Lab_5\File",  "lab\Lab_5\Log", "lab\Lab_5\office",  "lab\Lab_5\Window6"
+                                  'lab\Lab_5\Calculetion', "lab\Lab_5\File",  "lab\Lab_5\Log", "lab\Lab_5\office",  "lab\Lab_5\Window5",
                                   'lab\Lab_6\Calculetion', "lab\Lab_6\File",  "lab\Lab_6\Log", "lab\Lab_6\office",  "lab\Lab_6\Window6",
                                   "Main_window", "Sourse"],
                     'packages': [ "matplotlib", "pyqt5", "threading", "logging", "docxtpl", "shutil", "seaborn", "statistics",
